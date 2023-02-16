07eaa8e76eade8bf92f90a7aac9ab4108f4f9598
Muhammad Bilal <muhammad.bilal@postpay.io>
2023-02-16 15:53:46 +0500

    some new changes

---
+++ src/pages/Campaigns/index.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import CreateButton from "../../components/createButton";
+import {Heading} from "../../components/heading";
+import { CampaignTable } from "../../components/table";
+import "../../index.css";
+import CustomPagination from '../../components/pagination';
+
+export default function Campaigns() {
+  return (
+    <>
+      <div
+        className="tab-pane fade in active"
+        id="tasks-pill"
+        role="tabpanel"
+        aria-labelledby="tasks-pill-tab"
+      >
+        <main className="custom-main-content mt-2 pt-4">
+          <div className="flex justify-between items-center">
+            <Heading heading="All Campaigns" />
+            <CreateButton buttonName="Create"/>
+          </div>
+          <CampaignTable />
+        </main>
+        <CustomPagination count="1"/>
+      </div>
+    </>
+  );
+}
