57f053ceece6b63a84305e104ce150e72b2efc55
muhammad Bilal <ahmad_4al@hotmail.com>
2023-02-21 20:03:10 +0500

    Calander_UIFIxed

--- src/pages/Productivity/FullCalander/index.js
+++ src/pages/Productivity/FullCalander/index.js
@@ -8,30 +8,39 @@ export const MyCalendar = () => {
 
   return (
     <>
-    <div className="S_Form float-right">
-    <div className="input-group mb-3">
-  <div className="input-group-prepend">
-    <span className="input-group-text Icon" id="basic-addon1">
-        <i className="fa fa-search"></i>
-    </span>
-  </div>
-  <input type="text" className="form-control imput_Field" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1"/>
-</div>
-    </div>
+      <div className="S_Form float-right">
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text Icon" id="basic-addon1">
+              <i className="fa fa-search"></i>
+            </span>
+          </div>
+          <input type="text" className="form-control imput_Field" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1" />
+        </div>
+      </div>
 
-    <div>
-      <FullCalendar
-        editable
-        selectable
-        events={events}
-        headerToolbar={{
-          start: "prev today next",
-          end: "dayGridDay dayGridWeek dayGridMonth dayGridYear",
-        }}
-        plugins={[daygridPlugin, interactionPlugin]}
-        views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
-      />
-    </div>
+      <div>
+        <FullCalendar
+          editable
+          selectable
+          events={'https://fullcalendar.io/api/demo-feeds/events.json'}
+          headerToolbar={{
+            start: "prev today next",
+            end: "dayGridDay dayGridWeek dayGridMonth dayGridYear",
+          }}
+          plugins={[daygridPlugin, interactionPlugin]}
+          views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
+          eventContent={(arg) => {
+            return (
+              <div className="event-box p-2" >
+                <p className="m-0 font-12 text-Secondar-70 font-weight-400">ar</p>
+                <p className="m-0 font-12 text-Secondar-70 font-weight-600">assa</p>
+              </div>
+
+            )
+          }}
+        />
+      </div>
     </>
   );
 };
